573bb393628ddb2ec998bc3bbd0300b05b638665
Krzysztof Muniak <K.Muniak@student.tudelft.nl>
2024-05-05 22:59:32 +0200

    Implement WaterManagementSystem enviroment signature

--- core/envs/water_management_system.py
+++ core/envs/water_management_system.py
@@ -1,80 +1,77 @@
 import numpy as np
 import gymnasium as gym
-from gymnasium import spaces
-from gymnasium.core import Optional, ActType, ObsType
-from typing import List, SupportsFloat
+from gymnasium.spaces import Space, Dict
+from gymnasium.core import Optional, ObsType
+from typing import Any, List, Union, SupportsFloat
 from core.models.flow import Flow
-from core.models.facility import Facility
+from core.models.facility import Facility, ControlledFacility
 
 
 class WaterManagementSystem(gym.Env):
-    def __init__(self, facilities: List[Facility], flows: List[Flow]) -> None:
-        # TODO: decide on the type of observation space.
-        self.observation_space = gym.spaces.Box(low=0, high=100, shape=(1,))
-        
-        # TODO: decide on the type of action space.
-        self.action_space = gym.spaces.Discrete(2)
-        
-        # TODO: decide on the type of reward space.
-        self.reward_space = gym.spaces.Box(-np.inf, np.inf, shape=(1,))
+    def __init__(self, water_systems: List[Union[Facility, ControlledFacility, Flow]], seed=42) -> None:
+        self.water_systems: List[Union[Facility, ControlledFacility, Flow]] = water_systems
+        self.seed: int = seed
 
-    def _get_obs(self):
-        # TODO: decide on how we represent observation.
-        return {"agent": self._agent_location, "target": self._target_location}
-    
-    def _get_info(self):
+        self.observation_space: Space = self._determine_observation_space()
+        self.action_space: Space = self._determine_action_space()
+
+    def _determine_observation_space(self) -> Dict:
+        return Dict(
+            {
+                water_systems.id: water_systems.observation_space
+                for water_systems in self.facilities
+                if isinstance(water_systems, ControlledFacility)
+            },
+            self.seed,
+        )
+
+    def _determine_action_space(self) -> Dict:
+        return Dict(
+            {
+                water_systems.id: water_systems.action_space
+                for water_systems in self.water_systems
+                if isinstance(water_systems, ControlledFacility)
+            },
+            self.seed,
+        )
+
+    def _determine_info(self) -> dict[str, Any]:
         # TODO: decide on what we wnat to output in the info.
-        return {
-            "distance": np.linalg.norm(
-                self._agent_location - self._target_location, ord=1
-            )
-        }
-    
-    def reset(self, seed: Optional[int] = None, options: Optional[dict] = None) -> tuple[ObsType, dict]:
+        return {"facilites": self.facilities, "flows": self.flows}
+
+    def reset(self, seed: Optional[int] = None, options: Optional[dict] = None) -> tuple[ObsType, dict[str, Any]]:
         # We need the following line to seed self.np_random.
         super().reset(seed=seed)
-        self.time_step = 0
 
-        # TODO: Randomly initialize state of the enviroment.
+        for water_systems in self.water_systems:
+            water_systems.reset()
 
-        if self.initial_state is not None:
-            state = self.initial_state
-        else:
-            if not self.penalize:
-                state = self.np_random.choice(WaterManagementSystem.s_init, size=1)
+        return self._determine_observation_space(), self._determine_info()
+
+    def step(self, action: Dict) -> tuple[ObsType, SupportsFloat, bool, bool, dict]:
+
+        final_observation = None
+        final_reward = 0
+        final_terminated = False
+        final_truncated = False
+        final_info = None
+
+        for water_systems in self.water_systems:
+            if isinstance(water_systems, ControlledFacility):
+                observation, reward, terminated, truncated, info = water_systems.step(action[water_systems.id])
+            elif isinstance(water_systems, Facility) or isinstance(water_systems, Flow):
+                observation, reward, terminated, truncated, info = water_systems.step()
             else:
-                state = self.np_random.integers(0, 160, size=1)
+                raise ValueError()
 
-        self.state = np.array(state, dtype=np.float32)
-        
-        # observation = self._get_obs()
-        # info = self._get_info()
+            final_observation[water_systems.id] = observation
+            final_reward += reward
+            final_terminated = final_terminated or terminated
+            final_truncated = final_truncated or truncated
+            final_info[water_systems.id] = info
 
-        # return observation, info
+        return final_observation, final_reward, final_terminated, final_truncated, final_info
 
-        if self.render_mode == "human":
-            self.render()
-
-        return self.state, {}
-    
-    def step(self, action: ActType) -> tuple[ObsType, SupportsFloat, bool, bool, dict]:
-        # Map the action (element of {0,1,2,3}) to the direction we walk in
-        direction = self._action_to_direction[action]
-        # We use `np.clip` to make sure we don't leave the grid
-        self._agent_location = np.clip(
-            self._agent_location + direction, 0, self.size - 1
-        )
-        # An episode is done iff the agent has reached the target
-        terminated = np.array_equal(self._agent_location, self._target_location)
-        reward = 1 if terminated else 0  # Binary sparse rewards
-        observation = self._get_obs()
-        info = self._get_info()
-
-        if self.render_mode == "human":
-            self._render_frame()
-
-        return observation, reward, terminated, False, info
-    
     def close(self) -> None:
         # TODO: implement if needed, e.g. for closing opened rendering frames.
         pass
