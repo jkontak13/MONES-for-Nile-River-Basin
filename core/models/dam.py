c0f61aee757106cd1d487dfe4e5d6aa4d9494203
Krzysztof Muniak <K.Muniak@student.tudelft.nl>
2024-05-06 11:36:46 +0200

    Update dam stored water control

--- core/models/dam.py
+++ core/models/dam.py
@@ -1,8 +1,14 @@
 from core.models.facility import ControlledFacility
-from gymnasium.core import ObsType
+from gymnasium.spaces import Box, Space
 
 
 class Dam(ControlledFacility):
+    def __init__(
+        self, id: str, observation_space: Space, action_space: Box, max_capacity: float, stored_water: float = 0
+    ) -> None:
+        super().__init__(id, observation_space, action_space, max_capacity)
+        self.stored_water: float = stored_water
+
     def determine_reward(self) -> float:
         if self.stored_water > self.max_capacity:
             return float("-inf")
@@ -11,15 +17,22 @@ class Dam(ControlledFacility):
 
     def determine_outflow(self, action: float) -> float:
         # TODO: Determine correct action type.
-        return self.inflow * action
+        return action
 
     def determine_info(self) -> str:
         # TODO: Determine info for Dam.
         return ""
 
-    def determine_observation(self) -> ObsType:
+    def determine_observation(self) -> float:
         # TODO: Determine observation.
         return self.stored_water
 
     def is_terminated(self) -> bool:
-        return self.determine_reward() == float("-inf")
+        return self.stored_water > self.max_capacity or self.stored_water < 0
+
+    def step(self, action: float) -> tuple[float, float, bool, bool, dict]:
+        self.outflow = self.determine_outflow(action)
+        # TODO: Change stored_water to multiple outflows.
+        self.stored_water += self.inflow - self.outflow
+
+        return self.determine_observation(), self.determine_reward(), self.is_terminated(), False, self.determine_info()
--- core/models/facility.py
+++ core/models/facility.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
-from gymnasium.spaces import Space, Box
-from gymnasium.core import ObsType
+from gymnasium.spaces import Space
+from gymnasium.core import ObsType, ActType
 from typing import SupportsFloat
 
 
@@ -31,9 +31,7 @@ class Facility(ABC):
 
 
 class ControlledFacility(ABC):
-    def __init__(
-        self, id: str, observation_space: Space, action_space: Box, max_capacity: float, stored_water: float
-    ) -> None:
+    def __init__(self, id: str, observation_space: Space, action_space: ActType, max_capacity: float) -> None:
         self.id: str = id
         self.inflow: float = 0
         self.outflow: float = 0
@@ -42,14 +40,13 @@ class ControlledFacility(ABC):
         self.action_space: Space = action_space
 
         self.max_capacity: float = max_capacity
-        self.stored_water: float = stored_water
 
     @abstractmethod
     def determine_reward(self) -> float:
         raise NotImplementedError()
 
     @abstractmethod
-    def determine_outflow(self, action: Box) -> float:
+    def determine_outflow(self, action: ActType) -> float:
         raise NotImplementedError()
 
     @abstractmethod
@@ -64,9 +61,8 @@ class ControlledFacility(ABC):
     def is_terminated(self) -> bool:
         raise NotImplementedError()
 
-    def step(self, action: Box) -> tuple[ObsType, SupportsFloat, bool, bool, dict]:
+    def step(self, action: ActType) -> tuple[ObsType, SupportsFloat, bool, bool, dict]:
         self.outflow = self.determine_outflow(action)
         # TODO: Change stored_water to multiple outflows.
-        self.stored_water += self.inflow - self.outflow
 
         return self.determine_observation(), self.determine_reward(), self.is_terminated(), False, self.determine_info()
