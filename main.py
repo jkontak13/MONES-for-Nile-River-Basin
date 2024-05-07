6605af34eca8c91fe83e337d45b2263cc01662a7
Krzysztof Muniak <K.Muniak@student.tudelft.nl>
2024-05-07 16:43:16 +0200

    Add example catchment

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from core.models.dam import Dam
 from core.models.flow import Flow, Outflow, Inflow
 from core.models.power_plant import PowerPlant
 from core.models.irrigation_system import IrrigationSystem
+from core.models.catchment import Catchment
 
 
 power_plant = PowerPlant("power-plant", 1000, 0.2, 100)
 dam = Dam("dam", Box(0, 1000), 1000)
 irrigation_system = IrrigationSystem("irrigation-system", 1000, 500)
+catchment = Catchment("catchment", dam_irrigation_system_flow, 1000)
 
 power_plan_inflow = Inflow("inflow", power_plant, 1000)
 power_plant_dam_flow = Flow("power-plant-dam-flow", power_plant, dam, 1000)
-dam_irrigation_system_flow = Flow("dam_irrigation_system_flow", dam, irrigation_system, 1000)
+dam_irrigation_system_flow = Flow("dam_irrigation_system_flow", [dam, catchment], irrigation_system, 1000)
 irrigation_system_outflow = Outflow("outflow", dam, 1000)
 
 water_management_system = WaterManagementSystem(
