d6dc807bd390ff97537cba30adbe4a7db98d0f99
Krzysztof Muniak <K.Muniak@student.tudelft.nl>
2024-05-09 15:09:48 +0200

    Fix main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from gymnasium.spaces import Box, Dict
+from gymnasium.spaces import Box
 from core.envs.water_management_system import WaterManagementSystem
 from core.models.dam import Dam
 from core.models.flow import Flow, Outflow, Inflow
@@ -6,13 +6,13 @@ from core.models.power_plant import PowerPlant
 from core.models.irrigation_system import IrrigationSystem
 from core.models.catchment import Catchment
 
+# The numbers below are random and do not reflect actual constants of the Nile River simulation
+power_plant = PowerPlant("power-plant", 0.8, 1000, 1000, 1000, 0.2)
+dam = Dam("dam", Box(0, 1000), Box(0, 1000), 1000)
+irrigation_system = IrrigationSystem("irrigation-system", 500)
+catchment = Catchment("catchment", 1000)
 
-power_plant = PowerPlant("power-plant", 1000, 0.2, 100)
-dam = Dam("dam", Box(0, 1000), 1000)
-irrigation_system = IrrigationSystem("irrigation-system", 1000, 500)
-catchment = Catchment("catchment", dam_irrigation_system_flow, 1000)
-
-power_plan_inflow = Inflow("inflow", power_plant, 1000)
+power_plan_inflow = Inflow("inflow", power_plant, 1000, 1000)
 power_plant_dam_flow = Flow("power-plant-dam-flow", power_plant, dam, 1000)
 dam_irrigation_system_flow = Flow("dam_irrigation_system_flow", [dam, catchment], irrigation_system, 1000)
 irrigation_system_outflow = Outflow("outflow", dam, 1000)
