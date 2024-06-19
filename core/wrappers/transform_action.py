d792c383a28c73451080efb9316ab9269c27db28
Krzysztof Muniak <K.Muniak@student.tudelft.nl>
2024-06-19 22:44:08 +0200

    Fix reshape action wrapper

--- core/wrappers/transform_action.py
+++ core/wrappers/transform_action.py
@@ -2,26 +2,38 @@ import numpy as np
 import gymnasium as gym
 from gymnasium.spaces.dict import Dict
 from core.envs.water_management_system import WaterManagementSystem
+from core.models.facility import ControlledFacility
 
 
 class ReshapeArrayAction(gym.ActionWrapper, gym.utils.RecordConstructorArgs):
     def __init__(self, env: WaterManagementSystem):
         assert isinstance(env.action_space, Dict)
 
+        self.ordered_shapes = {}
         self.slices = {}
         current_index = 0
 
-        for name, sub_action_space in env.action_space.items():
-            self.slices[name] = slice(current_index, current_index + np.prod(sub_action_space.shape))
-            current_index += np.prod(sub_action_space.shape)
+        for water_system in env.water_systems:
+            if isinstance(water_system, ControlledFacility):
+                number_of_actions = np.prod(water_system.action_space.shape)
+
+                self.slices[water_system.name] = slice(current_index, current_index + number_of_actions)
+                self.ordered_shapes[water_system.name] = water_system.action_space.shape
+
+                current_index += number_of_actions
 
         gym.utils.RecordConstructorArgs.__init__(self)
         gym.ActionWrapper.__init__(self, env)
 
     def action(self, action):
+        print("Initial action: ", action)
+        print("Initial action type: ", type(action))
         reshaped_actions = {}
 
-        for name, sub_action_space in self.env.action_space.items():
-            reshaped_actions[name] = np.reshape(action[self.slices[name]], sub_action_space.shape)
+        for name, sub_action_space_shape in self.ordered_shapes.items():
+            reshaped_actions[name] = np.reshape(action[self.slices[name]], sub_action_space_shape)
+
+        print("Reshaped action: ", reshaped_actions)
+        print("Reshaped action type: ", type(reshaped_actions))
 
         return reshaped_actions
